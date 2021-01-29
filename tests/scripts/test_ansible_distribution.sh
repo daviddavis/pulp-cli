6e48d2c094cc685769f26eaca1a4dc3129652870
Matthias Dellweg <mdellweg@redhat.com>
2021-01-29 17:14:42 +0100

    Fix scoping for create
    [noissue]

--- tests/scripts/test_ansible_distribution.sh
+++ tests/scripts/test_ansible_distribution.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # shellcheck source=tests/scripts/config.source
-. "$(dirname "$(realpath "$0")")/config.source"
+. "$(dirname $(dirname "$(realpath "$0"))")/config.source"
 
 cleanup() {
   pulp ansible repository destroy --name "cli_test_ansible_repository" || true
